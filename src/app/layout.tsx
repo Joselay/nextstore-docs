1c601c01526d0ad1d485dc01bcc496713943fc9b
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2024-10-23 21:29:47 +0700

    last

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default async function RootLayout({
       (await import(`./${filename}`)).sections,
     ]),
   )) as Array<[string, Array<Section>]>
+
   let allSections = Object.fromEntries(allSectionsEntries)
 
   return (
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -232,25 +232,7 @@ function NavigationGroup({
 export const navigation: Array<NavGroup> = [
   {
     title: 'Guides',
-    links: [
-      { title: 'Introduction', href: '/' },
-      { title: 'Quickstart', href: '/quickstart' },
-      { title: 'SDKs', href: '/sdks' },
-      { title: 'Authentication', href: '/authentication' },
-      { title: 'Pagination', href: '/pagination' },
-      { title: 'Errors', href: '/errors' },
-      { title: 'Webhooks', href: '/webhooks' },
-    ],
-  },
-  {
-    title: 'Resources',
-    links: [
-      { title: 'Contacts', href: '/contacts' },
-      { title: 'Conversations', href: '/conversations' },
-      { title: 'Messages', href: '/messages' },
-      { title: 'Groups', href: '/groups' },
-      { title: 'Attachments', href: '/attachments' },
-    ],
+    links: [{ title: 'Introduction', href: '/' }],
   },
 ]
 
