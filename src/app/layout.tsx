f63d8dcf2f91cb0a0bef87705d24c88e5767ab15
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2024-10-24 00:32:20 +0700

    init docs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,6 @@ export default async function RootLayout({
       (await import(`./${filename}`)).sections,
     ]),
   )) as Array<[string, Array<Section>]>
-
   let allSections = Object.fromEntries(allSectionsEntries)
 
   return (
